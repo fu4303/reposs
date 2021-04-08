7bc7a512512157f3091410d52201a3f48ae65078
Michael Grigoryan <michael.grigoryan25@gmail.com>
2021-04-08 19:05:11 +0400

    Fixed a typo

--- src/app/utils/generateApiUrl.js
+++ src/app/utils/generateApiUrl.js
@@ -13,5 +13,5 @@ export const generateApiUrl = (
   per_page = 50,
   sortBy = "star"
 ) => {
-  return `https://api.github.com/search/repositories?q=stars>=${stars}&sort=${sortBy}&order=${order}&per_page=${per_page}`;
+  return `https://api.github.com/search/repositories?q=stars:>=${stars}&sort=${sortBy}&order=${order}&per_page=${per_page}`;
 };

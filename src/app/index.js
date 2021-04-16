3efa643772fc8254faa6bbe69ac6fac4f406369b
Michael Grigoryan <michael.grigoryan25@gmail.com>
2021-04-16 12:33:45 +0400

    Added Splitbee analytics

--- src/app/index.js
+++ src/app/index.js
@@ -1,6 +1,13 @@
 import theme from "./theme";
 import Routes from "./routes";
+import splitbee from "@splitbee/web";
 import { ChakraProvider } from "@chakra-ui/react";
+
+// Enabling Splitbee Analytics for non-development environments
+process.env.NODE_ENV !== "development" &&
+  splitbee.init({
+    token: process.env.REACT_APP_SPLITBEE_TOKEN,
+  });
 
 const App = () => {
   return (
